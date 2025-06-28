f03169972858e5c1a25c750934cea99df7db4ea2
Yanis <yanisamar2@gmail.com>
2025-06-28 19:27:45 +0200

    test health check

--- apps/frontend/hotel-intel/src/App.tsx
+++ apps/frontend/hotel-intel/src/App.tsx
@@ -1,10 +1,18 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  
+  useEffect(() => {
+    fetch('http://localhost:3000/health')
+      .then(res => res.json())
+      .then(data => console.log(data))
+      .catch(err => console.error(err));
+  }, []);
+
 
   return (
     <>
